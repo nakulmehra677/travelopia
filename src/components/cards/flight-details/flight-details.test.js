c33fbdea6f6e6b4938582704616da1fef812517e
nakulmehra677 <nakulmehra677@gmail.com>
2024-04-29 14:52:05 +0530

    modified test case

--- src/components/cards/flight-details/flight-details.test.js
+++ src/components/cards/flight-details/flight-details.test.js
@@ -1,32 +1,34 @@
 import { render } from "@testing-library/react";
-import CardFlightDetails from ".";
 import React from "react";
+import CardFlightDetails from ".";
 
-test("flight-details-data", () => {
-  const { container } = render(
-    <CardFlightDetails
-      data={{
-        flightNumber: "A1B46",
-        airline: "Airline 1",
-        origin: "Origin 1",
-        destination: "Destination 1",
-        departureTime: "28/4/2024, 3:00:57 PM",
-        status: "One Time",
-      }}
-    />
-  );
-  const cardElement = container.querySelector("#flight-details-data");
-  expect(cardElement.firstChild).toBeInTheDocument();
-});
+describe("flight-details", () => {
+  test("data", () => {
+    const { container } = render(
+      <CardFlightDetails
+        data={{
+          flightNumber: "A1B46",
+          airline: "Airline 1",
+          origin: "Origin 1",
+          destination: "Destination 1",
+          departureTime: "28/4/2024, 3:00:57 PM",
+          status: "One Time",
+        }}
+      />
+    );
+    const cardElement = container.querySelector("#flight-details-data");
+    expect(cardElement.firstChild).toBeInTheDocument();
+  });
 
-test("flight-details-loading", () => {
-  const { container } = render(<CardFlightDetails isLoading={true} />);
-  const cardElement = container.querySelector("#flight-details-loading");
-  expect(cardElement).toBeInTheDocument();
-});
+  test("loading", () => {
+    const { container } = render(<CardFlightDetails isLoading={true} />);
+    const cardElement = container.querySelector("#flight-details-loading");
+    expect(cardElement).toBeInTheDocument();
+  });
 
-test("flight-details-error", () => {
-  const { container } = render(<CardFlightDetails error={true} />);
-  const cardElement = container.querySelector("#flight-details-error");
-  expect(cardElement).toBeInTheDocument();
+  test("error", () => {
+    const { container } = render(<CardFlightDetails error={true} />);
+    const cardElement = container.querySelector("#flight-details-error");
+    expect(cardElement).toBeInTheDocument();
+  });
 });
--- src/components/table/flight-list/flight-list.test.js
+++ src/components/table/flight-list/flight-list.test.js
@@ -1,34 +1,34 @@
 import { render } from "@testing-library/react";
-import TableFlightList from ".";
 import React from "react";
+import TableFlightList from ".";
 
-test("flight-list-data", () => {
-  const { container } = render(
-    <TableFlightList
-      data={[
-        {
-          flightNumber: "A1B46",
-          airline: "Airline 1",
-          origin: "Origin 1",
-          destination: "Destination 1",
-          departureTime: "28/4/2024, 3:00:57 PM",
-          status: "One Time",
-        },
-      ]}
-    />
-  );
-  const cardElement = container.querySelector(".MuiTableBody-root");
-  expect(cardElement.firstChild).toBeInTheDocument();
-});
+describe("flight-list", () => {
+  test("data", () => {
+    const data = [
+      {
+        flightNumber: "A1B46",
+        airline: "Airline 1",
+        origin: "Origin 1",
+        destination: "Destination 1",
+        departureTime: "28/4/2024, 3:00:57 PM",
+        status: "One Time",
+      },
+    ];
 
-test("flight-list-loading", () => {
-  const { container } = render(<TableFlightList isLoading={true} />);
-  const cardElement = container.querySelector(".MuiTableBody-root");
-  expect(cardElement.firstChild).toBeInTheDocument();
-});
+    const { container } = render(<TableFlightList data={data} />);
+    const cardElement = container.querySelector(".MuiTableBody-root");
+    expect(cardElement.childElementCount).toBe(data.length);
+  });
 
-test("flight-list-error", () => {
-  const { container } = render(<TableFlightList error={true} />);
-  const cardElement = container.querySelector("#flight-list-error");
-  expect(cardElement).toBeInTheDocument();
+  test("loading", () => {
+    const { container } = render(<TableFlightList isLoading={true} />);
+    const cardElement = container.querySelector(".MuiTableBody-root");
+    expect(cardElement.firstChild).toBeInTheDocument();
+  });
+
+  test("error", () => {
+    const { container } = render(<TableFlightList error={true} />);
+    const cardElement = container.querySelector("#flight-list-error");
+    expect(cardElement).toBeInTheDocument();
+  });
 });
