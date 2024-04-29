582e7fa6b942c86b537461ed07c7a57ac98b263c
nakulmehra677 <nakulmehra677@gmail.com>
2024-04-29 21:13:33 +0530

    background

--- src/providers/theme-provider.js
+++ src/providers/theme-provider.js
@@ -1,15 +1,29 @@
-import { CssBaseline } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
-import React from "react";
 import PropTypes from "prop-types";
+import React from "react";
 
+import { PRIMARY } from "../styles/color";
 import theme from "../styles/theme";
 
 function ThemeProvider({ children }) {
   return (
     <MuiThemeProvider theme={theme}>
       <CssBaseline />
-      {children}
+      <Box position="relative">
+        {children}
+        <Box
+          position="fixed"
+          bottom={0}
+          right={0}
+          zIndex={-10}
+          sx={{ opacity: 0.8 }}
+          borderTop="160px solid transparent"
+          borderRight={`70vw solid ${PRIMARY.MAIN}`}
+          height={0}
+          width="50vw"
+        />
+      </Box>
     </MuiThemeProvider>
   );
 }
